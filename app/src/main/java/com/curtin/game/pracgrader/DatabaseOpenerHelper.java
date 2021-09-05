db535485285963bf849ed97658525ebd83b1ac18
oliramilo <olimarramilo@yahoo.com>
2021-09-05 18:25:51 +0800

    added images

--- app/src/main/java/com/curtin/game/pracgrader/DatabaseOpenerHelper.java
+++ app/src/main/java/com/curtin/game/pracgrader/DatabaseOpenerHelper.java
@@ -1,10 +1,27 @@
 package com.curtin.game.pracgrader;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseOpenerHelper extends SQLiteOpenHelper {
+import com.curtin.game.pracgrader.Database.MarksSchema;
+import com.curtin.game.pracgrader.Database.UserSchema;
 
-    public DatabaseOpenerHelper(){
-        super();
+public class DatabaseOpenerHelper extends SQLiteOpenHelper {
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "pracgrader.db";
+    public DatabaseOpenerHelper(Context context) {
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(UserSchema.UserTable.CREATE_TABLE);
+        sqLiteDatabase.execSQL(MarksSchema.MarksTable.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 }
